1d30aef055732a372915bd007b7021cf0f97ef0d
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2024-02-17 09:55:35 +0545

    util code discussion

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running at port: ${port}`);
+    });
+  })
+  .catch((error) => console.log("MongoDB connection failed!", error));
