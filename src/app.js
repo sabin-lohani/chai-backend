d2359db5750b68c0483a9619232dfe9c7174526b
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2024-02-18 17:07:41 +0545

    all setup configured

--- src/app.js
+++ src/app.js
@@ -9,4 +9,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.router.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
